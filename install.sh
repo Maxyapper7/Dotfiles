77e2a61cb5ccbb3f3272ef5a41433c64b1d1c355
Maxyapper7 <random.reshoot354@passinbox.com>
2024-06-16 11:35:21 -0600

    Added Twingate to setup

--- install.sh
+++ install.sh
@@ -20,5 +20,11 @@ sudo pacman -S -< pacman-pkg.txt
 # Install AUR packages
 paru -S --noconfirm -< aur-pkg.txt
 
+# Install Twingate
+curl -s https://binaries.twingate.com/client/linux/install.sh | sudo bash
+
 # Hyprlink dotfiles Using Stow
 stow alacritty flameshot picom tmux bottom polybar wallpapers dunst nvim rofi zsh dunst scripts i3 xresources
+
+# Set firefox as deafault browser
+xdg-settings check default-web-browser firefox.desktop
